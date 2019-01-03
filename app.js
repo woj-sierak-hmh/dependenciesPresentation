132ff37c56107b6dac63b3cddd949063d0fa4220
Sierakowski <wojciech.sierakowski@hmhco.com>
2019-01-03 17:13:49 +0000

    refactor: cleanup of code

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-/*
-Number of dependencies (50):
-$ npm ls --parseable | wc
-
-Number of lines of code in the app and its all dependencies (50779):
-$ find . -name '*.js' | xargs wc -l
-
-List of licenses in all depencies of the project (6/7):
-$ npx license-checker --summary
-*/
-
 const Twitter = require('twitter');
 
 const client = new Twitter({
